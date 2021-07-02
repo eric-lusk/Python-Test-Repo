487f40e9aa37c03766ed59b361b30dd3c6aa1bba
Eric Lusk <eric.m.lusk@outlook.com>
2021-07-02 01:39:46 -0400

    Exercise 1

---
+++ exercises.py
@@ -0,0 +1,11 @@
+# Exercise 1
+# print(2 + 1)
+# print(2 * 1)
+# print(3 / 2)
+# print(7 % 4)
+# print(20 % 2)
+# print(2 ** 3)
+# print(2 + 10 * 10 + 3)
+# print((2 + 10) * (10 + 3))
+# print(1 / 2)
+
